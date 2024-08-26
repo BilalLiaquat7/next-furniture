966e565821d99030e41c2fcc61d9e01ffe259b3f
bilalliaquat7 <bilal.liaquat7@gmail.com>
2024-08-27 01:03:33 +0500

    little changes made in navbar w.r.t mobile version but mobile menu left

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,17 @@ import Link from 'next/link'
 import React from 'react'
 import {usePathname}  from 'next/navigation';
 import classNames from 'classnames';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaCartShopping } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 
 const NavBar = () => {
 
+  let nav: boolean = true;
+  const handleNav = () => {
+    nav = !nav;
+    console.log(nav)
+  };
     const currentPath = usePathname();
 
     const links= [
@@ -17,27 +25,29 @@ const NavBar = () => {
     ]
 
   return (
-    <div className='border-b'>
-    <nav className='text-white flex justify-between h-24 items-center mx-12'>
-        <span className="text-xl flex items-center font-bold text-slate-900 cursor-pointer hover:text-pink-600">
-          Techways
-        </span>
+    <div className='border-b flex justify-between p-6 items-center'>
+      <GiHamburgerMenu onClick={()=> handleNav()} className='hover:cursor-pointer md:hidden'/>
+      <div className='md:text-xl items-center font-bold text-slate-900 cursor-pointer hover:text-pink-600'>
+        TECHWAYS
+      </div>
+      <nav className='hidden md:block text-white justify-between items-center mx-12'>
         <div className='flex justify-end items-center'>
-        <Link href='/'></Link>
-        <ul className='flex space-x-6'>
-            {
-                links.map((link) => 
-                <Link key={link.href}
-                className={classNames({
-                    'text-pink-600': link.href === currentPath,
-                    'text-zinc-900': link.href !== currentPath,
-                    'hover:text-pink-600 transition-colors': true
-                })} 
-                href={link.href}>{link.label}</Link>)
-            }
-        </ul>
+          <Link href='/'></Link>
+          <ul className='flex space-x-6'>
+          {
+            links.map((link) => 
+            <Link key={link.href}
+            className={classNames({
+              'text-pink-600': link.href === currentPath,
+              'text-zinc-900': link.href !== currentPath,
+              'hover:text-pink-600 transition-colors': true
+              })} 
+              href={link.href}>{link.label}</Link>)
+              }
+          </ul>
         </div>
-    </nav>
+      </nav>
+            <FaCartShopping/>
     </div>
   )
 }
